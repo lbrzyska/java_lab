4a83600147dc1255f88746919ccc716f1b9d4538
lbrzyska <92663788+lbrzyska@users.noreply.github.com>
2022-10-30 11:01:30 +0100

    Add files via upload

---
+++ lab1.1/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    System.out.println(getImie());
+    System.out.println(getWiek());
+    }
+    public static String getImie(){return "Linda";}
+    public static int getWiek(){return 20;}
+
+}
